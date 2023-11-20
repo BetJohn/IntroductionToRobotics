924e3693b1a44cf45b2799d292cc7c062bf93ed7
BetJohn <88667779+BetJohn@users.noreply.github.com>
2023-11-20 17:27:28 +0200

    Final code

--- Homework5/Homework5.ino
+++ Homework5/Homework5.ino
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3ab18d04bce3d04659e6fb273b4cd055f3d5b6090f43dd0024f6e6a66c52df44
-size 3077
+oid sha256:00bd8123bb1ca0cb8ac6ecb12929ba9d84d63f12bf02ef7a540fddcb6ed66c24
+size 11154
